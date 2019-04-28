5ae6813c97d881955969916886aff87480c9c3f6
uni51 <uni51@users.noreply.github.com>
2019-04-28 15:31:29 +0900

    ch05-03 クエリビルダ

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,27 +8,15 @@
 @endsection
 
 @section('content')
-    <p>{{$msg}}</p>
-    @if(count($errors) >0 )
-        <p>入力に問題があります。再入力してください。</p>
-    @endif
     <table>
-        <form action="/hello" method="post">
-            {{ csrf_field() }}
-            @if ($errors->has('name'))
-                <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
-            @endif
-            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
-            @if ($errors->has('mail'))
-                <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
-            @endif
-            <tr><th>mail: </th><td><input type="text" name="mail"value="{{old('mail')}}"></td></tr>
-            @if ($errors->has('age'))
-                <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
-            @endif
-            <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
-            <tr><th></th><td><input type="submit" value="send"></td></tr>
-        </form>
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
+            </tr>
+        @endforeach
     </table>
 @endsection
 
