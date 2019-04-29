84b12dfb606c5524b6a65f8f422e9a4d7fa945ce
uni51 <uni51@users.noreply.github.com>
2019-04-29 17:48:44 +0900

    ch06-03 モデルの保存・更新・削除

--- app/Person.php
+++ app/Person.php
@@ -2,39 +2,21 @@
 
 namespace App;
 
-//use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-//use PhpParser\Builder;
-use App\Scopes\ScopePerson;
 
 
 class Person extends Model
 {
-    protected static function boot()
-    {
-        parent::boot();
+    protected $guarded = array('id');
 
-        static::addGlobalScope(new ScopePerson);
-    }
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 
     public function getData()
     {
-        return $this->id . ': ' . $this->name . '(' . $this->age . ')';
-    }
-
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-
-
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
 }
