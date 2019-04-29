143e52f9b02ef5bf6c720f6310be53719690686d
uni51 <uni51@users.noreply.github.com>
2019-04-29 16:50:55 +0900

    ch06-02 検索とスコープ

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -19,8 +19,12 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)->ageLessThan($max)->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
+
+
 }
--- app/Person.php
+++ app/Person.php
@@ -2,11 +2,39 @@
 
 namespace App;
 
+//use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+//use PhpParser\Builder;
+use App\Scopes\ScopePerson;
+
 
 class Person extends Model
 {
-    public function getData(){
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ScopePerson);
+    }
+
+    public function getData()
+    {
         return $this->id . ': ' . $this->name . '(' . $this->age . ')';
+    }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
     }
 }
