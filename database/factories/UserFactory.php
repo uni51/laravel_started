e4c750bfee02962947054d04f0eaac6326afc694
uni51 <uni51@users.noreply.github.com>
2019-05-02 18:58:42 +0900

    ch07-05 ユニットテスト

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,12 +12,19 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Person::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'mail' => $faker->safeEmail,
+        'age' => random_int(1,999),
     ];
 });
